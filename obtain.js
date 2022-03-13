eb2b8b6cf6040f8e3d65e6b2734df9b679b2cd70
Declan <51113055+Dml7@users.noreply.github.com>
2022-03-14 10:43:38 +1300

    Update obtain.js

--- obtain.js
+++ obtain.js
@@ -2,22 +2,19 @@
 // I've decided to build this because I was bored...
 // You can only use this tool against specfic values if you have consent.
 const { Command } = require("commander");
+const { QuickScan } = require("node-nmap");
 const program = new Command();
 const { nmap, puppeteer, fs, readline } = require("./Modules/Modules.js");
 
 function banner() {
   console.log(`
 
-
-  _______ __     __          __        
+  _______ __     __          __
   |       |  |--.|  |_.---.-.|__|.-----.
   |   -   |  _  ||   _|  _  ||  ||     |
   |_______|_____||____|___._||__||__|__|
-                                        
+
       Type --help to display options
-
-
-
 
     `);
 }
@@ -28,29 +25,46 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-program
-  .option("-h, --host <host>", "Host Name/IP")
-  .option("-sh, --screenshot", "screenshot website")
-  .option("-nm, --nmap", "find out infomation from ports")
-  .action((host, options, command) => {
-    if (!options.host) {
-      // Host/url/ip
-      const HOST = host;
-      console.log(host);
-      rl.question("nmap or screenshot ?: ", function (nmap, screenshot) {
-        if (!options.screenshot) {
-          (async () => {
-            const browser = await puppeteer.launch();
-            const page = await browser.newPage();
-            await page.goto(`${host}`);
-            await page.screenshot({ path: "example.png" });
+const options = program.opts();
 
-            await browser.close();
-          })();
-        }
-        rl.close();
-      });
-    }
-  });
+program
+  .requiredOption("-h --host <host>", "Host Name/IP")
+  .option("-sh --screenshot", "screenshot website")
+  .option("-nm --nmap", "find out infomation from ports");
 
 program.parse(process.argv);
+
+const optionsHost = () => {
+  if (options.host && options.nmap) {
+    console.log(`Scanning... ${options.host}`);
+    runNmap();
+  } else if (options.host && options.screenshot) {
+    screenShot();
+  } else if (options.screenshot) {
+    screenshot();
+  } else {
+    console.log(`!! An error occured !!`);
+  }
+};
+
+const quickscan = new nmap.QuickScan(options.host);
+const runNmap = async () => {
+  await quickscan.on("complete", function (data) {
+    console.log(data[0]);
+  });
+};
+
+const screenShot = () => {
+  rl.question(`Enter targets url: `, function () {
+    (async () => {
+      const browser = await puppeteer.launch();
+      const page = await browser.newPage();
+      await page.goto("google.com");
+      await page.screenshot({ path: "example.png" });
+
+      await browser.close();
+    })();
+  });
+};
+
+optionsHost();
